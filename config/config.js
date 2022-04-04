b87274dad1a678db47b9f0e077e1ff341abbb9b3
LegrAnthony <aiupa.dev@gmail.com>
2022-04-04 20:55:25 +0200

    feat/ add linter

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
-const { Client, Intents } = require("discord.js");
-const { token } = require("./token.json");
+const { Client, Intents } = require('discord.js')
+const { token } = require('./token.json')
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+})
 
-client.on("messageCreate", async (message) => {
-    if (message.content.trim().toLowerCase() === "!ping") {
-        await message.channel.send("Pong !");
+client.on('messageCreate', async (message) => {
+    if (message.content.trim().toLowerCase() === '!ping') {
+        await message.channel.send('Pong !')
     }
-});
+})
 
-client.login(token);
+client.login(token)
