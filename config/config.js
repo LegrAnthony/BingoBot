199495d757bb4e11a2103639922617c20eb2147e
LegrAnthony <aiupa.dev@gmail.com>
2022-04-10 19:15:22 +0200

    feat/ add prefix

--- config/config.js
+++ config/config.js
@@ -1,13 +1,19 @@
 const { Client, Intents } = require('discord.js')
-const { token } = require('./token.json')
+const config = require('./config.json')
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 })
 
 client.on('messageCreate', async (message) => {
-    if (message.content.trim().toLowerCase() === '!ping') {
-        await message.channel.send('Pong !')
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return
+
+    if (message.content.startsWith(`${config.prefix}ping`)) {
+        message.channel.send('pong!')
+    } else
+
+    if (message.content.startsWith(`${config.prefix}kalten`)) {
+        message.channel.send('Qu\'il est moche')
     }
 })
 
-client.login(token)
+client.login(config.token)
